b4cf36ee184dacdc6c40e4182fd0c23c29ad3009
root <coucou@h3lp.fr>
2018-02-10 15:16:25 -0600

    Travail du samedi soir

--- libk/includes/libk.h
+++ libk/includes/libk.h
@@ -30,8 +30,10 @@ void printk(const char *s, ...);
 void	memcpy(void *dest, const void *src, size_t n);
 void	volatile_memcpy(void volatile *dest, const void volatile *src, size_t n);
 
+int		strcmp(const char *s1, const char *s2);
+
 // ASM functions
-extern "C"
-{
-}
+// extern "C"
+// {
+// }
 #endif
